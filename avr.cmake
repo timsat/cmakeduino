066e2b45f833dfd9c5bfafa74e163d25b60b7d44
Timur Sattarov <tim.helloworld@gmail.com>
2015-01-24 04:49:19 +0400

    initial commit

---
+++ avr.cmake
@@ -0,0 +1,23 @@
+SET(CMAKE_SYSTEM_NAME Generic)
+
+SET(CMAKE_C_COMPILER avr-gcc)
+SET(CMAKE_CXX_COMPILER avr-g++)
+
+SET(CSTANDARD "-std=gnu99")
+SET(CDEBUG "-gstabs")
+SET(CWARN "-Wall -Wstrict-prototypes")
+SET(CTUNING "-funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums")
+SET(COPT "-Os")
+SET(CINCS "-Iarduino")
+SET(CMCU "-mmcu=atmega32u4")
+SET(ARDUINO_VARIANT "leonardo")
+SET(CDEFS "-DF_CPU=16000000")
+
+SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
+SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
+
+SET(CFLAGS "${CMCU} ${CDEBUG} ${CDEFS} ${CINCS} ${COPT} ${CWARN} ${CSTANDARD} ${CEXTRA}")
+SET(CXXFLAGS "${CMCU} ${CDEFS} ${CINCS} ${COPT}")
+
+SET(CMAKE_C_FLAGS  ${CFLAGS})
+SET(CMAKE_CXX_FLAGS ${CXXFLAGS})
